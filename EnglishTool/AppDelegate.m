ac84bfb37cac48b39e6fc0cef9eaac44c8589994
MarkLiu <1144713192@qq.com>
2022-10-11 17:48:33 +0800

    修改

--- EnglishTool/AppDelegate.m
+++ EnglishTool/AppDelegate.m
@@ -16,6 +16,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    //仓库
+    
     return YES;
 }
 
